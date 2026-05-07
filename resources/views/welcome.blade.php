58596e010b030aca38d5b846f578231f933ee3ff
Joseph <mustopayusup12@gmail.com>
2026-05-07 11:21:54 +0700

    fix: landing page text visibility for light theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,13 @@
   
     <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
-        <div class="top-right links color-white">
+        <div class="top-right links">
             @auth
             <a href="{{ url('/admin') }}">Admin</a>
             @else
-            <a style="color: white" href="{{ route('login') }}">Login</a>
-
+            <a href="{{ route('login') }}" class="btn btn-primary text-white" style="padding: 8px 24px;">Login</a>
             @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
+            <a href="{{ route('register') }}" class="btn btn-outline-primary" style="padding: 8px 24px; margin-left: 10px;">Register</a>
             @endif
             @endauth
         </div>
@@ -19,7 +18,7 @@
             <div class="title m-b-md">
                 <div class="title m-b-md">
                     <div class="clockStyle" id="clock">123</div>
-                    <div id="date" style="color:#fff;font-size:24px;text-align:center;margin-top:10px;opacity:0.8;"></div>
+                    <div id="date" style="color: var(--text-secondary); font-size: 24px; text-align: center; margin-top: 10px; font-weight: 500;"></div>
                 </div>            
             </div>
         </div>
