8745d7388b97e60b23ff8cbebfe417717df5e6fa
Joseph <mustopayusup12@gmail.com>
2026-05-11 09:32:58 +0700

    UI: Centered right navbar vertically, removed fullscreen button, and added Indonesian flag image

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,34 +14,27 @@
 </div>
 
 <nav class="navbar-custom">
-    <ul class="navbar-right d-flex list-inline float-right mb-0">
+    <ul class="navbar-right d-flex list-inline float-right mb-0" style="align-items: center; height: 100%; margin-top: -3px;">
         <!-- language-->
         <li class="dropdown notification-list d-none d-md-block">
-            <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                <img src="/assets/images/flags/us_flag.jpg" class="mr-2" height="12" alt="" onerror="this.style.display='none'"/> English <span class="mdi mdi-chevron-down"></span>
+            <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false" style="display: flex; align-items: center; padding-top: 0; padding-bottom: 0;">
+                <img src="/assets/images/flags/us_flag.jpg" class="mr-2" height="12" alt="" onerror="this.style.display='none'"/> English <span class="mdi mdi-chevron-down ml-1"></span>
             </a>
             <div class="dropdown-menu dropdown-menu-right language-switch">
                 <a class="dropdown-item" href="#"><img src="/assets/images/flags/us_flag.jpg" alt="" height="16" onerror="this.style.display='none'"/><span> English </span></a>
-                <a class="dropdown-item" href="#"><img src="/assets/images/flags/indonesia_flag.jpg" alt="" height="16" onerror="this.style.display='none'"/><span> Indonesia </span></a>
+                <a class="dropdown-item" href="#"><img src="/assets/images/flags/indonesia_flag.png" alt="" height="16" onerror="this.style.display='none'"/><span> Indonesia </span></a>
             </div>
         </li>
 
         <!-- dark mode toggle -->
         <li class="dropdown notification-list d-none d-md-block">
-            <a class="nav-link waves-effect" href="#" id="btn-dark-mode" title="Toggle Dark Mode">
+            <a class="nav-link waves-effect" href="#" id="btn-dark-mode" title="Toggle Dark Mode" style="display: flex; align-items: center; padding-top: 0; padding-bottom: 0;">
                 <i class="mdi mdi-moon-waning-crescent noti-icon"></i>
             </a>
         </li>
-
-        <!-- full screen -->
-        <li class="dropdown notification-list d-none d-md-block">
-            <a class="nav-link waves-effect" href="#" id="btn-fullscreen" title="Full Screen">
-                <i class="mdi mdi-fullscreen noti-icon"></i>
-            </a>
-        </li>
-        <li class="dropdown notification-list">
+        <li class="dropdown notification-list" style="display: flex; align-items: center;">
             <div class="dropdown notification-list nav-pro-img">
-                <a class="dropdown-toggle nav-link arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                <a class="dropdown-toggle nav-link arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false" style="display: flex; align-items: center; padding-top: 0; padding-bottom: 0;">
                     <img src="assets/images/profile1.png" alt="user" class="rounded-circle">
                 </a>
                 <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
